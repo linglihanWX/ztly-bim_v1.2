c558fee4aede30e50a0ef92cc98558614293d237
freedo <freedo@MSI>
2018-01-02 16:21:48 +0800

    点击设置图标跳转至设置页面控制器加注释

--- src/main/java/com/freedotech/app/ztly/web/ShezhiController.java
+++ src/main/java/com/freedotech/app/ztly/web/ShezhiController.java
@@ -3,17 +3,10 @@ package com.freedotech.app.ztly.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
-* <p>Title: ShezhiController</p>
-* <p>Description: 跳转至编码库控制器</p>
-* <p>Company: freedo</p> 
-*  @author freedoxiaolong 
-   @date 2018年1月2日*/
 @Controller
 @RequestMapping("/Shezhi")
 public class ShezhiController {
- 
-	//跳转至编码库页面
+
 	@RequestMapping("/toBianmaku")
 	public String toBianmakuPagePage() {
 		return "bianmaku";
