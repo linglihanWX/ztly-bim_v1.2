dcc5412779d300b8c167e049568cae2586860316
linglihanWX <823532365@qq.com>
2018-01-26 13:21:51 +0800

    添加了管片破损页面的三维初始化方法

--- src/main/webapp/static/page/dungou/guanpianposui/js/guanpianposui3D.js
+++ src/main/webapp/static/page/dungou/guanpianposui/js/guanpianposui3D.js
@@ -1,5 +1,90 @@
 $(function(){
 	 //初始化地球
     FreedoApp.init("earth");
-    
+    cameraControl(FreedoApp.viewers["earth"]);
+    var pmodels = [];
+    $.ajax({
+        url: "../../PModel/getPmodel",
+        type: "get",
+        dataType: "json",
+        success: function (data) {
+            //解析json
+            var model = eval(data);
+            for (var key in model) {
+                //挖坑数据
+                var holeData = eval(model[key].hole);
+                //图层数据
+                var imgarray = eval(model[key].imagelayer);
+                //挖坑
+                if (holeData != null && imgarray != null) {
+                    //获取地址栏地址
+                    var path = window.location.pathname;
+                    //截取字符串
+                    var patharray = path.split("/");
+                    //拼接文件地址
+                    for (var i = 0; i < imgarray.length; i++) {
+                        var str = "";
+                        for (var j = 0; j < patharray.length - 3; j++) {
+                            str = str + "../"
+                        }
+                        imgarray[i] = str + imgarray[i];
+                    }
+                    //调用挖坑方法
+                    FreeDoUtil.dig(FreedoApp.viewers["earth"], holeData, imgarray);
+                }
+                console.log(model[key].url);
+                //向场景中添加模型
+                var modelTile = FreedoApp.viewers["earth"].scene.primitives.add(new FreeDo.FreedoPModelset({
+                    url: model[key].url
+                }));
+                var index = model[key].unitname;
+                pmodels[index] = modelTile;
+                if (model[key].x != 0 || model[key].y != 0 || model[key].z != 0 || model[key].heading != 0 || model[key].pitch != 0 || model[key].roll != 0 || model[key].scalex != 1 || model[key].scaley != 1 || model[key].scalez != 1) {
+                    //调整模型位置
+                    modelTile.readyPromise.then(function (modeltile) {
+                        moveModel(modeltile, model[key].x, model[key].y, model[key].z, model[key].heading, model[key].pitch, model[key].roll, model[key].scalex, model[key].scaley, model[key].scalez);
+                    });
+                }
+                if (model[key].cameralon != null || model[key].cameralat != null || model[key].cameraheight != null || model[key].cameraheading != null || model[key].camerapitch != null || model[key].cameraroll != null) {
+
+                    //镜头定位
+                    FreedoApp.viewers["earth"].camera.setView({
+                        destination: new FreeDo.Cartesian3.fromDegrees(model[key].cameralon, model[key].cameralat, model[key].cameraheight),
+                        orientation: {
+                            heading: model[key].cameraheading,
+                            pitch: model[key].camerapitch,
+                            roll: model[key].cameraroll
+                        }
+                    });
+                    $("#resetview").click(function () {
+                        FreedoApp.viewers["earth"].camera.setView({
+                            destination : new FreeDo.Cartesian3.fromDegrees(model[key].cameralon, model[key].cameralat, model[key].cameraheight),
+                            orientation : new FreeDo.HeadingPitchRoll(model[key].cameraheading,model[key].camerapitch,model[key].cameraroll)
+                        });
+                    })
+                } else {
+                    modelTile.readyPromise.then(function () {
+                        FreedoApp.viewers["earth"].camera.flyToBoundingSphere(modelTile.boundingSphere,{
+                            duration:0
+                        });
+                    });
+                    $("#resetview").click(function () {FreedoApp.viewers["earth"].camera.flyToBoundingSphere(modelTile.boundingSphere,{
+                        duration:0
+                    });});
+                }
+                pmodel = modelTile;
+                if(model[key].name=="dalian2"){
+                    for (var i = 2; i <=9668; i+=18) {
+                        allready.push(["${component} ~==  \'"+i+"\'", 'color("green")'])
+                    }
+                    allready.push(['true', 'color("white")'])
+                    pmodel.style = new FreeDo.FreedoPModelStyle({
+                        color : {
+                            conditions : allready
+                        }
+                    });
+                }
+            }
+        }
+    });
 });
