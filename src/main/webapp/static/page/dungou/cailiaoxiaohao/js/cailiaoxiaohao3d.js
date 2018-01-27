f4aeeaea0552fd2403f54ac21adaaba1ed03e0c9
linglihanWX <823532365@qq.com>
2018-01-27 16:39:18 +0800

    添加了材料消耗的三维页面加载项目的方法

--- src/main/webapp/static/page/dungou/cailiaoxiaohao/js/cailiaoxiaohao3d.js
+++ src/main/webapp/static/page/dungou/cailiaoxiaohao/js/cailiaoxiaohao3d.js
@@ -1,5 +1,134 @@
+var pmodel = {}
+var allready = [];
 $(function(){
 	 //初始化地球
     FreedoApp.init("earth");
     cameraControl(FreedoApp.viewers["earth"]);
+    var pmodels = [];
+    $.ajax({
+        url: "../../PModel/getPmodel",
+        type: "get",
+        dataType: "json",
+        success: function (data) {
+            //解析json
+            var model = eval(data);
+            for (var key in model) {
+                //挖坑数据
+                var holeData = eval(model[key].hole);
+                //图层数据
+                var imgarray = eval(model[key].imagelayer);
+                //挖坑
+                if (holeData != null && imgarray != null) {
+                    //获取地址栏地址
+                    var path = window.location.pathname;
+                    //截取字符串
+                    var patharray = path.split("/");
+                    //拼接文件地址
+                    for (var i = 0; i < imgarray.length; i++) {
+                        var str = "";
+                        for (var j = 0; j < patharray.length - 3; j++) {
+                            str = str + "../"
+                        }
+                        imgarray[i] = str + imgarray[i];
+                    }
+                    //调用挖坑方法
+                    FreeDoUtil.dig(FreedoApp.viewers["earth"], holeData, imgarray);
+                }
+                console.log(model[key].url);
+                //向场景中添加模型
+                var modelTile = FreedoApp.viewers["earth"].scene.primitives.add(new FreeDo.FreedoPModelset({
+                    url: model[key].url
+                }));
+                var index = model[key].unitname;
+                pmodels[index] = modelTile;
+                if (model[key].x != 0 || model[key].y != 0 || model[key].z != 0 || model[key].heading != 0 || model[key].pitch != 0 || model[key].roll != 0 || model[key].scalex != 1 || model[key].scaley != 1 || model[key].scalez != 1) {
+                    //调整模型位置
+                    modelTile.readyPromise.then(function (modeltile) {
+                        moveModel(modeltile, model[key].x, model[key].y, model[key].z, model[key].heading, model[key].pitch, model[key].roll, model[key].scalex, model[key].scaley, model[key].scalez);
+                    });
+                }
+                if (model[key].cameralon != null || model[key].cameralat != null || model[key].cameraheight != null || model[key].cameraheading != null || model[key].camerapitch != null || model[key].cameraroll != null) {
+
+                    //镜头定位
+                    FreedoApp.viewers["earth"].camera.setView({
+                        destination: new FreeDo.Cartesian3.fromDegrees(model[key].cameralon, model[key].cameralat, model[key].cameraheight),
+                        orientation: {
+                            heading: model[key].cameraheading,
+                            pitch: model[key].camerapitch,
+                            roll: model[key].cameraroll
+                        }
+                    });
+                    $("#resetview").click(function () {
+                        FreedoApp.viewers["earth"].camera.setView({
+                            destination : new FreeDo.Cartesian3.fromDegrees(model[key].cameralon, model[key].cameralat, model[key].cameraheight),
+                            orientation : new FreeDo.HeadingPitchRoll(model[key].cameraheading,model[key].camerapitch,model[key].cameraroll)
+                        });
+                    })
+                } else {
+                    modelTile.readyPromise.then(function () {
+                        FreedoApp.viewers["earth"].camera.flyToBoundingSphere(modelTile.boundingSphere,{
+                            duration:0
+                        });
+                    });
+                    $("#resetview").click(function () {FreedoApp.viewers["earth"].camera.flyToBoundingSphere(modelTile.boundingSphere,{
+                        duration:0
+                    });});
+                }
+                pmodel = modelTile;
+                if(model[key].name=="dalian2"){
+                    //初始化模型的颜色，用来显示已经盾构的环和没有盾构的环
+                    for (var i = 2; i <=9668; i+=18) {
+                        allready.push(["${component} ~==  \'"+i+"\'", 'color("aquamarine",0.5)'])
+                    }
+                    allready.push(['true', 'color("white")'])
+                    pmodel.style = new FreeDo.FreedoPModelStyle({
+                        color : {
+                            conditions : allready
+                        },
+                        show :{
+                            conditions : [
+                                ["${component} ~==  \'9668\'", 'false'],
+                                ['true','true']
+                            ]
+                        }
+                    });
+
+                    //盾构机旋转
+/*                    var pitch = 0;
+                    FreedoApp.viewers["earth"].scene.preRender.addEventListener(function(){
+                        if(pitch>360)pitch=0;
+                        pitch = pitch+1;
+                        primitive.modelMatrix = FreeDoTool.getModelMatrix(121.62022781066331, 38.93872856969979,-23,287,pitch,0,1.2,1.2,1.2);
+
+                    });*/
+                    //加盾构机和盾构机机身
+                    var daotou = FreedoApp.viewers["earth"].scene.primitives.add(FreeDo.Model.fromGltf(
+                        {
+                            id: "盾构机刀头",
+                            url: "http://182.92.7.32:9000/ztly/glb/dungoujidaotou/dun_gou_dao_tou.gltf",
+                            show: true,                     // default
+                            modelMatrix:FreeDoTool.getModelMatrix(121.62022781066331, 38.93872856969979,-491.5,165,0,0,1.4,1.4,1.4),
+                            allowPicking: true,            // not pickable
+                            debugShowBoundingVolume: false, // default
+                            debugWireframe: false
+                    }));
+
+                    var cheshen = FreedoApp.viewers["earth"].scene.primitives.add(FreeDo.Model.fromGltf(
+                        {
+                            id: "盾构机车身",
+                            url: "http://182.92.7.32:9000/ztly/glb/cheshen.glb",
+                            show: true,                     // default
+                            modelMatrix:FreeDoTool.getModelMatrix(121.62022781066331, 38.93872856969979,-491.5,249,6,0,1.2,1.2,1.2),
+                            allowPicking: true,            // not pickable
+                            debugShowBoundingVolume: false, // default
+                            debugWireframe: false
+                     }));
+                    cheshen.color = FreeDo.Color.RED;
+/*                    modelTile.readyPromise.then(function(){
+                    console.log(FreeDoTool.getSphereFromBoundsMinMax("5480.149836637288900,-255.272293286915470,-4820.024217262735900","5486.746820721270500,-240.645764969164760,-4805.037810776616400",pmodel));
+                    })*/
+                }
+            }
+        }
+    });
 });
