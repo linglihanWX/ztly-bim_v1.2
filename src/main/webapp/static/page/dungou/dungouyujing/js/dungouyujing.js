fa401727ca8767ac7e1763aab4767adae5c675e4
linglihanWX <823532365@qq.com>
2018-02-07 09:44:12 +0800

    调整了数据顺序，删除了数据中多余的空格，添加了排序的方法

--- src/main/webapp/static/page/dungou/dungouyujing/js/dungouyujing.js
+++ src/main/webapp/static/page/dungou/dungouyujing/js/dungouyujing.js
@@ -43,6 +43,7 @@ $(function(){
         // checkbox:true,
         // treeField:'project',
         rownumbers:true,
+        remoteSort:false,
         // pageList: [10,20,30],
         // pagination:true,
         columns: [[
@@ -53,8 +54,45 @@ $(function(){
             { title: '偏差类型', field: 'deviationtype', align: 'center'},
             { title: '偏差数据', field: 'deviationdata', align: 'center'},
             { title: '预警等级', field: 'grade', align: 'center'},
-            { title: '治理状态', field: 'stake', align: 'center'},
-            { title: '填报日期', field: 'time', align: 'center'},
+            { title: '治理状态', field: 'stake', align: 'center',
+            	styler:function (value,row,index) {
+            		console.log(value);
+                if(value == "已清除"){
+                    return "color:green;"
+                }else if(value == "进行中"){
+                    return "color:orange;"
+                }else if(value == "待整改"){
+                    return "color:red;"
+                }
+             },sortable: true,sorter:function(a,b){
+                 if(a == "已清除"){
+                     return 1;
+             }else if(a == "进行中"){
+                 if(b == "已清除"){
+                     return -1;
+                 }else if(b == "进行中" || b == "待整改"){
+                     return 1;
+                 }
+             }else if(a == "待整改"){
+                 return -1;
+             }
+
+         }},
+            { title: '填报日期', field: 'time', align: 'center',
+        	 sortable: true,sorter:function(c,d){
+                 let a = c.split('-');
+                 let b = d.split('-');
+                 if (a[0] == b[0]){
+                     if (a[1] == b[1]){
+                         return (a[2]>b[2]?1:-1);
+                     } else {
+                         return (a[1]>b[1]?1:-1);
+                     }
+                 } else {
+                     return (a[0]>b[0]?1:-1);
+                 }
+             }
+},
             { title: '填报人', field: 'menName', align: 'center'},
             { title: '预览', field: 'mark', align: 'center',styler: function(value,row,index){
                 return 'color:blue;cursor:pointer;text-decoration:underline;';
